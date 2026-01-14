a97e0bec2a54eca56f5d295481dbb06de5355e7f
Patrick Lupton <patricklupton74@gmail.com>
2026-01-14 19:40:54 +0000

    First attempt at adding unit propagation, unsuccessful

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+####################### (↓) INPUT PARSING & SANITATION #######################
+
 def lettersToNumbers(cnf):
     #loops through cnf array of arrays
     for x in range(len(cnf)):
@@ -43,6 +45,8 @@ def parseCNF(input):
     lettersToNumbers(cnf)  
     return cnf
 
+####################### (↑) INPUT PARSING & SANITATION #######################
+
 input = str(input("enter a CNF. "))
 
 #parse input, split into array of arrays representing a CNF and its disjunctive clauses
@@ -50,3 +54,37 @@ cnf = parseCNF(input)
 
 print(cnf)
 
+def simplify(cnf, trues):
+    #unit propagation (clauses with exactly 1 proposition must be true)
+    #loop through cnf array of arrays (clauses), seeing if any clauses have only 1 proposition 
+    for x in range(len(cnf)):
+        if len(cnf[x]) == 1:
+            #if trues already contains inverse of this clause then return contradiction
+            if ((cnf[x][0]) * -1) in trues:
+                return False
+            else:
+                trues.append((cnf[x][0]))
+                #deals with new truth assignment
+                for y in range(len(cnf)):
+                    #if clause is depedent, make it empty
+                    if cnf[x][0] in cnf[y]:
+                        cnf[y] = []
+                    #if clause has inverse, remove the inverse, leaving only other propositions in that clause
+    #remove all empty clauses
+    for x in range(len(cnf)):
+        if cnf[len(cnf) - x - 1] == []:
+            cnf.pop(len(cnf) - x - 1)
+    return True
+
+def DPLL(cnf):
+    trues = []
+    if simplify(cnf, trues):
+        print(cnf)
+        print(trues)
+    else:
+        print("unsat")
+        print(cnf)
+        print(trues)
+
+DPLL(cnf)
+    
